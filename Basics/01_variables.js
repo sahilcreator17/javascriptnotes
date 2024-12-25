e6d225b47b37e637afa6c8f3ee3489432524696a
sahilcreator17 <sahilsainijbp10@gmail.com>
2024-12-25 11:12:39 +0530

    Basic vairables and data types

---
+++ Basics/01_variables.js
@@ -0,0 +1,45 @@
+/*
+    This is for variables
+
+    Variables = Memory location where we save or store a value
+*/
+
+
+// Variables defined by 2 ways - 1. let 2. const 3. var(not used anymore)
+
+
+// 1. let = it will allow you to store any value and also allow you to change the value in future
+
+
+let firstName = "Avinash"
+
+// to display in terminal we use console.log()
+
+console.log(firstName)
+
+// Note -- whenever you define a variable you can not redefine it
+
+// firstName = 23
+
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
+
+
+// 2. const = it will allow you to create but never allow you to change its value in future
+
+const age = 32
+
+console.log(age);
+
+// age = 44
+
+// console.log(age);
+
+// 3. var = it will allow you to create and change its value in future
+
+// var lastName = "Dubey"
+
+// This is not used because of global scope and block scope
+
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,38 @@
+// Datatypes - 1. Primitive (Non reference) 2. Non Primitive (Reference)
+
+/*
+    1. Primitive Data types - 
+        a. Number = 2^53
+        b. String = set of charachters(spaces will be counted)
+        c. Boolean = true or false(0/1)
+        d. Bigint = greater 2^53
+        e. Symbol = unique data type
+*/
+
+// 1. Number 
+
+let a = 10
+const b = 20
+
+let c = a+b
+
+// console.log(c);
+
+// 2. String
+
+let firstName = "Avinash"
+let lastName = "Dubey"
+
+let fullName = firstName + lastName // string concatination 
+
+// console.log(fullName); // AvinashDubey
+
+// 3. Boolean
+
+let isValid = false
+
+// console.log(isValid);
+
+// Note -- try to use is with the variable name like isValid , isPasswordCorrect for boolean value
+
+
