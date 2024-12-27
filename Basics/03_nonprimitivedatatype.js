b53f5054f73a34811bb2311af5ba08c44e2fb068
sahilcreator17 <sahilsainijbp10@gmail.com>
2024-12-27 11:07:13 +0530

    non primitive

---
+++ Basics/03_nonprimitivedatatype.js
@@ -0,0 +1,37 @@
+
+// Non Primitive Data type 
+
+/*
+    1. Array
+    2. Object
+    3. Function
+*/
+
+//1. Array = a collection of multiple data and datatypes
+
+let arr1 = [1, 2, 3, 4, "Sam", "Atul", true]
+
+// console.log(arr1);
+
+// console.log(arr1[1]);
+
+
+// similarity between string and array
+// let firstName = "Avinash"
+// console.log(firstName[2]);
+
+let arr2 = arr1
+
+// console.log(arr2);
+
+arr2 = [1,2,3,4]
+
+// console.log(arr2);
+// console.log(arr1);
+
+
+let arr3 = [1,2,3,[1,2,[5,6,7]]]
+
+// console.log(arr3[3][2][0]);
+// console.log(arr3.flat(Infinity));
+
