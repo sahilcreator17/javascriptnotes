d3c0c5b0de1221083d419b45b946d82b602704d2
sahilcreator17 <sahilsainijbp10@gmail.com>
2025-01-02 11:46:51 +0530

    controlflow

---
+++ Basics/04_controlflow.js
@@ -0,0 +1,54 @@
+// Loops and If-else statements
+
+// Loops - anything that needs to be repeated
+
+// for (variable, condition, variable update) {
+    
+// }
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);   
+// }
+
+// console.log(i)
+
+// let i = 0
+
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 20
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+
+// for(let i = 0; i <= 10; i++){
+//     if(i == 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let i = 30
+// if(i >= 20){
+//     console.log("i is bigger than 20");
+// }else {
+//     console.log("i is smaller than 20"); 
+// }  
+
+
+
+for(let i = 1; i <= 10; i++){
+    if(i ==6) {
+        break;
+    }
+
+    
+    console.log(i*5);   
+}
+
