b216b06549078765852ebad475d234e2d0526332
sahilcreator17 <sahilsainijbp10@gmail.com>
2025-01-06 14:05:04 +0530

    new

---
+++ Basics/06.scope.js
@@ -0,0 +1,26 @@
+// Global scope
+
+// let a = 10
+
+// functional scope
+
+// for(let i = 0; i<10; i++){
+//     console.log(i);    
+// }
+
+// console.log(i);
+
+// Eval scope -- just like global scope but in different location
+// mongoose, prisma, drizzle
+
+// let a = 10
+// let b = 20
+
+// let c
+
+// function Sum(a,b){
+//     c = a+b;
+//     console.log(c);
+// }
+
+// Sum(10,20)
